f4a3e3c6843518ad91f5487dc2fa8f30b4c842a0
NataLira1 <cavalcantenatan28@gmail.com>
2024-04-27 23:31:02 -0300

    Main Principal finalizada

---
+++ prolog/mainPrincipal.pl
@@ -0,0 +1,114 @@
+:- set_prolog_flag(encoding, utf8).
+:- module(mainPrincipal, [main/0]).
+
+:- use_module(util).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(thread)).
+
+:- initialization(main).
+
+main :-
+    limpar_terminal,
+    writeln('_________________________________________________________'),
+    writeln('               Seja Bem-vindo a CodeFit                  '),
+    writeln('_________________________________________________________'),
+    writeln('|   1. ALUNO'),
+    writeln('|   2. GESTOR'),
+    writeln('|   3. FUNCIONÁRIO'),
+    writeln('|'),
+    write('| > Tipo de usuario (''!'' Para Sair): '),
+    flush_output,
+    read_line_to_codes(user_input, TipoUsuarioCodes),
+    atom_chars(TipoUsuario, TipoUsuarioCodes), 
+    
+    (TipoUsuario = '!' ->
+        writeln("Encerrando..."),
+        halt
+    ;
+        atom_number(TipoUsuario, TipoUsuarioInt),
+        tipo_usuario_correto(TipoUsuarioInt, TipoUsuarioValidado),
+        (   TipoUsuarioValidado =:= 1 ->
+            writeln('login_aluno')
+        ;   loginMembro(TipoUsuarioValidado)
+        
+        )
+
+    ).
+
+
+    atom_number(TipoUsuario, TipoUsuarioInt),
+    tipo_usuario_correto(TipoUsuarioInt, TipoUsuarioValidado),
+        (   TipoUsuarioValidado =:= 1 ->
+            writeln('login_aluno')
+        ;   loginMembro(TipoUsuarioValidado)
+        
+        ).
+       
+loginMembro(TipoUsuario):-
+    limpar_terminal,
+    writeln('_________________________________________________________'),
+    writeln('               Seja Bem-vindo a CodeFit                  '),
+    writeln('_________________________________________________________'),
+    writeln('__________________________LOGIN__________________________'),
+    writeln('Digite o seu cpf (! para sair): '),
+    read(CPF),
+
+
+    (CPF = '!' ->
+        writeln("Encerrando..."),
+        halt
+    ;
+        writeln('Digite sua senha: '),
+        read(Senha),
+        writeln('Carregando ...'),
+        validadorLogin(CPF, TipoUsuario, Senha),
+        sleep(2)
+
+    ).
+
+
+%Função para validar se login já existe
+validadorLogin(Cpf, TipoUsuarioV, SenhaV):-
+    atom_string(SenhaV, SenhaString),
+    concat_atom(["BD/login/", Cpf, ".json"], Path),
+    (exists_file(Path) -> 
+        (
+            ler_json(Path, _{cpf : CPFj, senha: Senhaj, tipoUsuario: Tipoj}),
+            (Senhaj = SenhaString, Tipoj = TipoUsuarioV ->
+            writeln("LOGIN CADASTRADO"),
+            menu_usuario(TipoUsuarioV)
+            ;
+            writeln("SENHA OU TIPO DE USUARIO INVALIDOS, TENTE NOVAMENTE!"),
+            sleep(3),
+            loginMembro(TipoUsuarioV)
+            )
+        )
+    ;
+        writeln("LOGIN NÃO CADASTRADO,TENTE NOVAMENTE!"),
+        sleep(3),
+        main
+    ).
+
+%Função para chamar menus   
+menu_usuario(TipoUsuario) :-
+    (
+        TipoUsuario =:= 2 ->
+            writeln('MENU GESTOR')
+        ;   TipoUsuario =:= 3 ->
+            writeln('MENU FUNCIONARIO')
+    ).
+
+
+tipo_usuario_correto(X, X) :-
+    verificar_int_tipo_funcionario(X).
+tipo_usuario_correto(_, TipoCorreto) :-
+    writeln('Opção inválida, tente novamente!'),
+    read(NovaOpcao),
+    tipo_usuario_correto(NovaOpcao, TipoCorreto).
+
+verificar_int_tipo_funcionario(X) :-
+    between(1, 3, X).
+
+
+
